29d21ca3b6afccbfcea7e561ffe5903c0096ee27
DmitryMorozov62 <62morozov.dima.90@gmail.com>
2023-03-14 19:06:48 +0300

    Initial commit

---
+++ DmitryCalculatorApplication.java
@@ -0,0 +1,13 @@
+package DmitryCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmitryCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmitryCalculatorApplication.class, args);
+	}
+
+}
